8f9d4dd16b0c95145f39d72f50e93054810cf056
Jiajie Chen <c@jia.je>
2020-06-04 13:47:40 +0800

    Re-impl miller-rabin with num-bigint

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,82 @@
 #![feature(const_generics)]
 #![allow(incomplete_features)]
 
-pub mod big_int;
+use num_bigint::*;
+use rand;
 
-pub fn miller_rabin<const COUNT: usize>(num: &big_int::BigUInt<COUNT>, t: usize) -> bool {
+/// Run Miller-Rabin primality test for `t` rounds
+pub fn miller_rabin<T: ToBigUint>(num: &T, t: usize) -> bool {
+    let n = num.to_biguint().unwrap();
+    let zero: BigUint = 0usize.into();
+    let one: BigUint = 1usize.into();
+    let two: BigUint = 2usize.into();
+    let n1 = &n - &one;
+
     // trvial cases
-    if num < &big_int::BigUInt::<COUNT>::two() {
+    if n <= one {
         return false;
-    } else if num == &big_int::BigUInt::<COUNT>::two() || num == &big_int::BigUInt::<COUNT>::three()
-    {
-        return false;
+    } else if n == two || n == 3usize.into() {
+        return true;
     }
 
     // find r and d such that n = 2^r*d+1
-    let mut d = num.clone() - big_int::BigUInt::<COUNT>::one();
     let mut r = 0;
-    while d.low_bit() {
+    let mut d: BigUint = n.clone() - &one;
+    while (&d & &one) == zero {
+        d >>= 1;
         r += 1;
-        d = d >> 1;
+    }
+
+    // try t times
+    let mut rng = rand::thread_rng();
+    for _ in 0..t {
+        // pick a random integer a in the range [2, n − 2]
+        let a = rng.gen_biguint(n1.bits());
+        if a >= two && a < n1 {
+            // x ← a^d mod n
+            let mut x = a.modpow(&d, &n);
+            // if x = 1 or x = n − 1 then
+            if x == one || x == n1 {
+                //   continue WitnessLoop
+                continue;
+            }
+
+            let mut prime = false;
+            // repeat r − 1 times:
+            for _ in 0..r - 1 {
+                // x ← x^2 mod n
+                x = x.modpow(&two, &n);
+                // if x = n − 1 then
+                if x == n1 {
+                    // continue WitnessLoop
+                    prime = true;
+                    break;
+                }
+            }
+
+            if prime {
+                continue;
+            } else {
+                // composite
+                return false;
+            }
+        }
     }
     true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TIMES: usize = 100;
+
+    #[test]
+    fn test_miller_rabin() {
+        assert!(miller_rabin(&2, TIMES));
+        assert!(miller_rabin(&3, TIMES));
+        assert!(miller_rabin(&5, TIMES));
+        assert!(miller_rabin(&7, TIMES));
+
+        assert!(!miller_rabin(&4, TIMES));
+    }
 }
