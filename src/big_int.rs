3890740668f170ac68324533b2abc5bc3b932cc0
Jiajie Chen <c@jia.je>
2020-06-04 12:26:38 +0800

    Add more functions for BigUInt

--- src/big_int.rs
+++ src/big_int.rs
@@ -1,8 +1,9 @@
 use std::cmp::Ordering;
 use std::fmt;
-use std::ops::Add;
+use std::ops::{Add, Shl};
 
 type Unit = u64;
+const BITS: usize = std::mem::size_of::<Unit>() * 8;
 
 #[derive(Clone)]
 pub struct BigUInt<const COUNT: usize> {
@@ -18,6 +19,42 @@ impl<const COUNT: usize> BigUInt<COUNT> {
     pub fn one() -> BigUInt<COUNT> {
         let mut res = BigUInt { num: [0; COUNT] };
         res.num[0] = 1;
+        res
+    }
+
+    pub fn two() -> BigUInt<COUNT> {
+        let mut res = BigUInt { num: [0; COUNT] };
+        res.num[0] = 2;
+        res
+    }
+
+    pub fn is_zero(&self) -> bool {
+        for i in 0..COUNT {
+            if self.num[i] != 0 {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn lowbit(&self) -> bool {
+        (self.num[0] & 1) == 1
+    }
+
+    pub fn from_hex_str(input: &str) -> Self {
+        let mut res = Self { num: [0; COUNT] };
+        for (i, ch) in input.bytes().rev().enumerate() {
+            let num = if b'0' <= ch && ch <= b'9' {
+                ch - b'0'
+            } else if b'a' <= ch && ch <= b'z' {
+                ch - b'a'
+            } else if b'A' <= ch && ch <= b'Z' {
+                ch - b'Z'
+            } else {
+                0
+            } as Unit;
+            res.num[i / (BITS / 4)] |= num << ((i % (BITS / 4)) * 4);
+        }
         res
     }
 }
@@ -45,6 +82,38 @@ impl<const COUNT: usize> Add<Unit> for BigUInt<COUNT> {
                 if !overflow {
                     break;
                 }
+            }
+        }
+        self
+    }
+}
+
+impl<const COUNT: usize> Add<BigUInt<COUNT>> for BigUInt<COUNT> {
+    type Output = BigUInt<COUNT>;
+
+    fn add(mut self, other: BigUInt<COUNT>) -> Self {
+        let mut carry = 0;
+        for i in 0..COUNT {
+            let (new, overflow) = self.num[i].overflowing_add(other.num[i]);
+            let (new, overflow2) = new.overflowing_add(carry);
+            self.num[i] = new;
+            carry = (overflow || overflow2) as Unit;
+        }
+        self
+    }
+}
+
+impl<const COUNT: usize> Shl<usize> for BigUInt<COUNT> {
+    type Output = BigUInt<COUNT>;
+
+    fn shl(mut self, mut other: usize) -> Self {
+        // only sub word shifting is supported yet
+        other = other % BITS;
+        if other != 0 {
+            self.num[COUNT - 1] <<= other;
+            for i in (0..COUNT - 1).rev() {
+                self.num[i + 1] |= self.num[i] >> (BITS - other);
+                self.num[i] <<= other;
             }
         }
         self
@@ -85,7 +154,7 @@ impl<const COUNT: usize> PartialOrd for BigUInt<COUNT> {
 
 impl<const COUNT: usize> fmt::Debug for BigUInt<COUNT> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let octets = std::mem::size_of::<Unit>() * 8 / 4;
+        let octets = BITS / 4;
         write!(f, "0x")?;
         for i in (0..COUNT).rev() {
             let s = format!("{:X}", self.num[i]);
@@ -106,6 +175,36 @@ mod tests {
         assert_eq!(BigUInt::<C>::zero(), BigUInt::<C>::zero());
         assert_eq!(BigUInt::<C>::one(), BigUInt::<C>::one());
         assert_ne!(BigUInt::<C>::zero(), BigUInt::<C>::one());
-        assert_eq!(BigUInt::<C>::zero(), BigUInt::<C>::one());
+        assert_ne!(BigUInt::<C>::zero(), BigUInt::<C>::one());
+    }
+
+    #[test]
+    fn add() {
+        assert_eq!(BigUInt::<C>::zero() + 1, BigUInt::<C>::one());
+        assert_eq!(BigUInt::<C>::zero() + 2, BigUInt::<C>::two());
+        assert_eq!(BigUInt::<C>::one() + 0, BigUInt::<C>::one());
+        assert_eq!(BigUInt::<C>::one() + 1, BigUInt::<C>::two());
+        assert_eq!(
+            BigUInt::<C>::one() + BigUInt::<C>::one(),
+            BigUInt::<C>::two()
+        );
+    }
+
+    #[test]
+    fn shl() {
+        assert_eq!(BigUInt::<C>::zero() << 1, BigUInt::<C>::zero());
+        assert_eq!(BigUInt::<C>::zero() << 2, BigUInt::<C>::zero());
+        assert_eq!(BigUInt::<C>::one() << 0, BigUInt::<C>::one());
+        assert_eq!(BigUInt::<C>::one() << 1, BigUInt::<C>::two());
+        assert_eq!(BigUInt::<C>::one() << 3, BigUInt::<C>::two() << 2);
+    }
+
+    #[test]
+    fn from_hex_str() {
+        assert_eq!(BigUInt::<C>::from_hex_str("0"), BigUInt::<C>::zero());
+        assert_eq!(BigUInt::<C>::from_hex_str("1"), BigUInt::<C>::one());
+        assert_eq!(BigUInt::<C>::from_hex_str("2"), BigUInt::<C>::two());
+        assert_eq!(BigUInt::<C>::from_hex_str("10"), BigUInt::<C>::one() << 4);
+        assert_eq!(BigUInt::<C>::from_hex_str("100"), BigUInt::<C>::one() << 8);
     }
 }
